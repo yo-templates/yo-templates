17696ecb64fda0b7b2bc9cdbfdeebce2a21628c6
yo-templates <55458872+yo-templates@users.noreply.github.com>
2019-09-17 18:54:09 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,2 @@
+$(document).ready(function () { if ($("#creditme,.creditme").attr("href") != "https://yo-templates.blogspot.com/") {
+        window.location.href = "https://yo-templates.blogspot.com/"; }});
